ada297a4e46c691926d9eba0e69474183b6d3772
10khatri <f1footballfreestylers@gmail.com>
2024-08-14 11:19:02 +0530

    add login form

--- src/Pages/login/login.tsx
+++ src/Pages/login/login.tsx
@@ -1,13 +1,96 @@
+import { Layout, Card, Space, Input, Form, Checkbox, Button, Flex } from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
+import Logo from "../../components/icons/Logo";
 export default function LoginPage() {
   return (
     <>
-      <h1>Sign in</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log in</button>
-      <label htmlFor="remember me">Remember me</label>
-      <input type="checkbox" id="remember me" />
-      <a href="#">Forgot password</a>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space
+          direction="vertical"
+          size={"large"}
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          <Layout.Content>
+            <Logo />
+          </Layout.Content>
+          <Card
+            bordered={false}
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  fontSize: 16,
+                  justifyContent: "center",
+                  width: "100%",
+                }}
+              >
+                <LockFilled /> Sign in
+              </Space>
+            }
+          >
+            <Form initialValues={{ remember: true }}>
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                  {
+                    type: "email",
+                    message: "Please input a valid email!",
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                  {
+                    min: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Flex style={{ justifyContent: "space-between" }}>
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="#" id="login-form-forgot">
+                  Forgot password
+                </a>
+              </Flex>
+
+              <Form.Item name="password">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "antd/dist/reset.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./router.tsx";
+import { ConfigProvider } from "antd";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={Router} />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#F65F42",
+          colorLink: "#F65F42",
+        },
+      }}
+    >
+      <RouterProvider router={Router} />
+    </ConfigProvider>
   </StrictMode>
 );
